df64aa192df0a4a4220bbdee4e2c06357a0acf63
Muazzam Younus <muazzam.younus925@gmail.com>
2020-07-13 21:53:55 +0500

    Updates

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+
+        <div class="container__wrapper">
+            <post-component></post-component>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import PostComponent from './partials/PostComponent';
+
+    export default {
+        name: "MainComponent",
+        components: {
+            PostComponent,
+        },
+        mounted() {
+            console.log('Main Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/partials/PostComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="post">
+        <div class="post__col post__col--three-fourth">
+            <div class="post__header">
+                <h2 class="post__title">10 Best Photo Apps For Incredible iPhone Photography (2020 Edition)</h2>
+                <span class="post__title__border"></span>
+            </div>
+
+            <div class="post__body post__body__main">
+                <div class="post__description">
+                    <p>How do you know which ones to choose? To help you decide, we’ve compiled a list of the 10 best
+                        photography apps to improve your iPhone photos. Read on to discover the best photo apps for your
+                        needs. Are you confused about which apps to use for your iPhone photography? With so many apps
+                        available, it can be difficult to know which ones are the best. To help you out, we’ve compiled
+                        an overview of the top 100 most popular and effective iPhone photography apps. Whether you’re a
+                        beginner or a seasoned pro, this list will help you choose the best apps for shooting, editing,
+                        managing, sharing and selling your iPhone photos.</p>
+                </div>
+            </div>
+        </div>
+        <div class="post__col post__col--one-fourth">
+            <div class="post__header">
+            </div>
+
+            <div class="post__body post__body__sidebar post__body--gray">
+                <button class="btn btn--gradiant">
+                    <span class="btn__text">Send Me the Tips</span>
+                </button>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "PostComponent",
+        mounted() {
+            console.log('Post Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
